60a09feb5b85c958b0a8755613fbd2d6899a2c37
GingaSystem <37409683+GingaSystem@users.noreply.github.com>
2020-09-20 01:54:01 +0900

    ビジュアライザを二つにしてWebカメラ機能を足しました

---
+++ Oculus Demo/Assets/Scripts/WebCam.cs
@@ -0,0 +1,29 @@
+﻿    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    // Webカメラ
+    public class WebCam : MonoBehaviour
+    {
+        private static int INPUT_SIZE = 256;
+        private static int FPS = 30;
+
+        // UI
+        RawImage rawImage;
+        WebCamTexture webCamTexture;
+
+        // スタート時に呼ばれる
+        void Start()
+        {
+            // Webカメラの開始
+            this.rawImage = GetComponent<RawImage>();
+    #if UNITY_EDITOR_OSX
+            this.webCamTexture = new WebCamTexture("FaceTime HD Camera");
+    #else
+    this.webCamTexture = new WebCamTexture();
+    #endif
+            this.rawImage.texture = this.webCamTexture;
+            this.webCamTexture.Play();
+        }
+    }
---
+++ Oculus Demo/Assets/Scripts/WebcamCapture.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WebcamCapture : MonoBehaviour
+{
+
+    //このクラスをウェブカメラの映像をテクスチャとして貼り付けるオブジェクトに適用する
+
+    private WebCamTexture webcamtex;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        webcamtex = new WebCamTexture();   //コンストラクタ
+
+        Renderer _renderer = GetComponent<Renderer>();  //Planeオブジェクトのレンダラ
+        _renderer.material.mainTexture = webcamtex;    //mainTextureにWebCamTextureを指定
+        webcamtex.Play();  //ウェブカムを作動させる
+    }
+
+}
