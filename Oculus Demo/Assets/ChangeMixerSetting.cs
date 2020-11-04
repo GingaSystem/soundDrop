9b9bdc2def20f649078c83f1872e35f87a27171c
GingaSystem <37409683+GingaSystem@users.noreply.github.com>
2020-11-04 23:25:59 +0900

    宙に浮くボックスをイコライザに反映

--- Oculus Demo/Assets/ChangeMixerSetting.cs
+++ Oculus Demo/Assets/ChangeMixerSetting.cs
@@ -4,30 +4,17 @@ using UnityEngine;
 
 public class ChangeMixerSetting : MonoBehaviour
 {
+    public GameObject testCube;
     public GameObject cube;
+    public GameObject cube2;
+    public GameObject cube3;
+    public GameObject cube4;
+    public GameObject cube5;
     // Start is called before the first frame update
     void Start()
     {
-        float gain1 = 0,gain2 = 0,gain3 = 0,gain4 = 0,gain5 = 0;
-        var aud = GetComponent<AudioSource>();
-    
-        Debug.Log(gain1);
-        gain1 = cube.transform.localScale.y;
-        gain2 = cube.transform.localScale.y;
-        gain3 = cube.transform.localScale.y;
-        gain4 = cube.transform.localScale.y;
-        gain5 = cube.transform.localScale.y;
-
-        
-
-        Debug.Log("cubeScale"+cube.transform.localScale);
 
 
-        aud.outputAudioMixerGroup.audioMixer.SetFloat("gain1", gain1);
-        aud.outputAudioMixerGroup.audioMixer.SetFloat("gain2", gain2);
-        aud.outputAudioMixerGroup.audioMixer.SetFloat("gain3", gain3);
-        aud.outputAudioMixerGroup.audioMixer.SetFloat("gain4", gain4);
-        aud.outputAudioMixerGroup.audioMixer.SetFloat("gain5", gain5);
 
 
 
@@ -39,6 +26,27 @@ public class ChangeMixerSetting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        float gain1 = 0, gain2 = 0, gain3 = 0, gain4 = 0, gain5 = 0;
+        var aud = GetComponent<AudioSource>();
+
+        Debug.Log(gain1);
+        gain1 = cube.transform.localScale.y;
+        gain2 = cube2.transform.localScale.y;
+        gain3 = cube3.transform.localScale.y;
+        gain4 = cube4.transform.localScale.y;
+        gain5 = cube5.transform.localScale.y;
+
+
+
+        Debug.Log("cubeScale" + cube.transform.localScale);
+
+
+        aud.outputAudioMixerGroup.audioMixer.SetFloat("gain1", gain1);
+        aud.outputAudioMixerGroup.audioMixer.SetFloat("gain2", gain2);
+        aud.outputAudioMixerGroup.audioMixer.SetFloat("gain3", gain3);
+        aud.outputAudioMixerGroup.audioMixer.SetFloat("gain4", gain4);
+        aud.outputAudioMixerGroup.audioMixer.SetFloat("gain5", gain5);
 
     }
 }
