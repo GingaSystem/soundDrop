c844b78b478dad62a983e689d635725ba284bd52
GingaSystem <37409683+GingaSystem@users.noreply.github.com>
2020-12-16 15:15:09 +0900

    ヴィジュアライズ関数の模索(途中)

--- Oculus Demo/Assets/Scripts/WaterFallVisualizerController.cs
+++ Oculus Demo/Assets/Scripts/WaterFallVisualizerController.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class WaterFallVisualizerController : MonoBehaviour
@@ -32,7 +34,7 @@ public class WaterFallVisualizerController : MonoBehaviour
             */
 
             // (2) Round
-            wf.transform.localPosition = Quaternion.Euler(0, 180.0f * ((float)i / spectrum.Levels.Length), 0 ) * wf.transform.localPosition;
+            wf.transform.localPosition = Quaternion.Euler(0, 180.0f * ((float)i / spectrum.Levels.Length), 0) * wf.transform.localPosition;
 
             // --- color
             var trailsModule = wf.GetComponent<ParticleSystem>().trails;
@@ -62,7 +64,41 @@ public class WaterFallVisualizerController : MonoBehaviour
         {
             var waterFall = waterFalls[i];
             var emission = waterFall.emission;
-            emission.rateOverTime = 3.0f + spectrum.Levels[i] * scale;
+            //この関数で水の量を調整
+            //emission.rateOverTime = 0.5f + spectrum.Levels[i] * scale;
+            //emission.rateOverTime = 3 ^ (int)(spectrum.Levels[i] * scale);
+            //emission.rateOverTime = (int)(spectrum.Levels[i] * 3000) ^ (int)(spectrum.Levels[i] * scale);
+            //emission.rateOverTime = 30;
+
+            if (spectrum.Levels[i] <= 0.0005)
+            {
+                emission.rateOverTime = 0;
+            }
+            else if (spectrum.Levels[i] < 0.03)
+            {
+                //spectrum.Levels[i]が0から0.0022まで y=a^spectrum.Levels[i]*10000 指数関数
+                //emission.rateOverTime = Mathf.Pow(1.11f, spectrum.Levels[i]);
+                emission.rateOverTime = Mathf.Pow(1.0008f, spectrum.Levels[i]);
+            }
+            /*
+            else if (spectrum.Levels[i] < 0.03)
+            {
+                //spectrum.Levels[i]が0.0022から0.02まで y = ax + b：y = 188.4973315x + 21.23005337 
+                emission.rateOverTime = 188.4973315f * spectrum.Levels[i] + 21.23005337f-10;
+            }
+            */
+            else if (spectrum.Levels[i] < 0.072)
+            {
+
+                //spectrum.Levels[i]が0.02から0.072まで y = ax + b：y = 96.15384615x + 23.07692308
+                emission.rateOverTime = 96.15384615f * spectrum.Levels[i] + 23.07692308f-10;
+
+            }
+            else { emission.rateOverTime = 20; }
+
+            //Debug.Log(spectrum.Levels[i]);
+            //File.AppendAllText("/tmp/out.txt", i + "," + spectrum.Levels[i] + "\n");
+
         }
 
         // Reflect current gain level (determined based on the collision status) to the mixer.
